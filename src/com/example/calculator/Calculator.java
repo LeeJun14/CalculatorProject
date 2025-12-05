e372b1141e116b3dc7e4713de8fcd34ed0fa7b83
Leejun14 <777ljy777@gmail.com>
2025-12-05 15:36:07 +0900

    양의 정수(0 포함)를 입력받기

---
+++ src/com/example/calculator/Calculator.java
@@ -0,0 +1,15 @@
+package com.example.calculator;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first integer: ");
+        int a = sc.nextInt();
+        System.out.print("Enter second integer: ");
+        int b = sc.nextInt();
+    }
+}
