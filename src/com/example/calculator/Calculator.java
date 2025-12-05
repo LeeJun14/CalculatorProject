b648545b68e2212c81ba7dc6d221fa3cec35da46
Leejun14 <777ljy777@gmail.com>
2025-12-05 17:47:02 +0900

    사칙연산 기호 입력받기

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -6,10 +6,20 @@ public class Calculator {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        int result = 0;
+        String reseltMessage = "";
 
-        System.out.print("Enter first integer: ");
+
+        System.out.print("첫 번째 값을 입력해주세요: ");
         int a = sc.nextInt();
-        System.out.print("Enter second integer: ");
+        System.out.print("두 번째 값을 입력해주세요: ");
         int b = sc.nextInt();
+        System.out.print("연산기호를 선택해주세요: ");
+        char Operator = sc.next().charAt(0);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("Operator = " + Operator);
+
+
     }
 }
