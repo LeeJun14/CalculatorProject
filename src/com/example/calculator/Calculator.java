94777574ed4db9fba39cee3eb5f3253c33848efd
Leejun14 <777ljy777@gmail.com>
2025-12-08 14:34:12 +0900

    Calculator 클래스 생성 및 기존 클래스 이름 변경

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,63 +1,28 @@
 package com.example.calculator;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Calculator {
+    ArrayList<Integer> list = new ArrayList<Integer>();
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int result = 0;
-
-        while (true) {
-            System.out.print("첫 번째 값을 입력해주세요: ");
-            int a = sc.nextInt();
-            System.out.print("두 번째 값을 입력해주세요: ");
-            int b = sc.nextInt();
-            System.out.print("연산기호를 선택해주세요: ");
-            char Operator = sc.next().charAt(0);
-
-            boolean flag = true;
-
-            switch (Operator) {
-                case '+':
-                    result = a + b;
-                    break;
-
-                case  '-':
-                    result = a - b;
-                    break;
-
-                case  '*':
-                    result = a * b;
-                    break;
-
-                case '/':
-                    if(b == 0) {
-                        flag = false;
-                        System.out.println("나눗셈 연산에서 분모에 0이 입력될 수 없습니다.");
-                    } else {
-                        result = a / b;
-                        break;
-                    }
-
-
-                default:
-                    System.out.println("유효하지 않은 연산기호입니다.");
-                    flag = false;
-                    break;
-            }
-            if(!flag) {
-                continue;
-            } else {
-                System.out.println(result);
-            }
-            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
-            String exit = sc.next();
-            if(exit.equals("exit")) {
+    public int calculate(int a, int b, char Operator) {
+        switch (Operator) {
+            case '+':
+                list.add(a + b);
                 break;
-            } else {
-                flag = false;
-            }
+
+            case  '-':
+                list.add(a - b);
+                break;
+
+            case   '*':
+                list.add(a * b);
+                break;
+
+            case  '/':
+                list.add(a / b);
+                break;
         }
+        return list.get(list.size()-1);
     }
 }
